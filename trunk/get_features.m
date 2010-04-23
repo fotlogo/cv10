838916aebba0aeb2b737d18185cb246c755bce1e
edwardsj@cs.utexas.edu <edwardsj@cs.utexas.edu@4277bcfc-06df-3f71-2080-f62be5d74ad5>
2010-04-23 20:50:03 +0000

    Added color and texture features

--- trunk/get_features.m
+++ trunk/get_features.m
@@ -9,8 +9,8 @@ global img_dir hog_dir tc_dir;
 hogfn = fullfile(hog_dir, sprintf('%s_hog.mat', img_name));
 hog = load(hogfn);
 
-%tcfn = fullfile(tc_dir, sprintf('%s_tc.mat', img_name));
-%tc = load(tcfn);
+tcfn = fullfile(tc_dir, sprintf('%s_tc.mat', img_name));
+tc = load(tcfn);
 
 % If the mask is a bounding box, use it as a bounding box
 if (size(mask, 2) == 4)
@@ -21,31 +21,35 @@ if (size(mask, 2) == 4)
   feat_hog = hist(hog.idx(hog_ind),1:1000);
   feat_hog_norm = feat_hog/norm(feat_hog);
 
-  %feat_color = hist(tc.colorim(box(2):box(4), box(1):box(3)),1:128);
-  %feat_color_norm = feat_color/norm(feat_color);
+  feat_color = hist(tc.colorim(box(2):box(4), box(1):box(3)),1: ...
+		    128);
+  feat_color = sum(feat_color');
+  feat_color_norm = feat_color/norm(feat_color);
 
-  %feat_texture = hist(tc.textonim(box(2):box(4), box(1):box(3)),1:256);
-  %feat_texture_norm = feat_texture/norm(feat_texture);
+  feat_texture = hist(tc.textonim(box(2):box(4), box(1):box(3)),1:256);
+  feat_texture = sum(feat_texture');
+  feat_texture_norm = feat_texture/norm(feat_texture);
 
 else
   % This is the case where the mask is a true mask.
   hog_ind = logical(arrayfun(@(x,y)mask(y,x), uint8(hog.x), uint8(hog.y)));
-  %disp(sprintf('%u', hog_ind(1:10:end, 1:10:end)'));
 
   feat_hog = hist(hog.idx(hog_ind),1:1000);
-  %disp(sprintf('%u', feat_hog(1:10:end, 1:10:end)'));
   if (nnz(feat_hog) > 0)
     feat_hog_norm = feat_hog/norm(feat_hog);
   else
     feat_hog_norm = feat_hog;
   end
 
-  %feat_color = hist(tc.colorim(box(2):box(4), box(1):box(3)),1:128);
-  %feat_color_norm = feat_color/norm(feat_color);
+  feat_color = hist(tc.colorim(box(2):box(4), box(1):box(3)),1:128);
+  feat_color_norm = feat_color/norm(feat_color);
 
-  %feat_texture = hist(tc.textonim(box(2):box(4), box(1):box(3)),1:256);
-  %feat_texture_norm = feat_texture/norm(feat_texture);
+  feat_texture = hist(tc.textonim(box(2):box(4), box(1):box(3)),1:256);
+  feat_texture_norm = feat_texture/norm(feat_texture);
 
 end
 
-feat = feat_hog_norm; %[feat_hog_norm feat_color_norm feat_texture_norm];
+%size(feat_hog_norm)
+%size(feat_color_norm)
+%size(feat_texture_norm)
+feat = [feat_hog_norm feat_color_norm feat_texture_norm];
